3649c8ad0cf054c2b70d7ad7c545d532e0613678
Mykola Osadchyi <kol.osadchiy@gmail.com>
2025-05-17 19:10:36 +0300

    modified conftest.py

--- conftest.py
+++ conftest.py
@@ -4,16 +4,10 @@ from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
 
 
-@pytest.fixture(scope="module")
+@pytest.fixture(scope="session")
 def driver():
     service = Service(executable_path=ChromeDriverManager().install())
-
-    # set up the Chrome options
-    options = webdriver.ChromeOptions()
-    options.add_argument("--start-maximized")
-    # options.headless = True
-
-    driver = webdriver.Chrome(service=service, options=options)
+    driver = webdriver.Chrome(service=service)
 
     yield driver
 
